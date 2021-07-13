5a2651d6a2f13275911dcccc000f8bc471e02f3c
Thiago <thiago.ralves02@gmail.com>
2021-07-13 11:33:49 -0300

    final front

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+class Musica:
+    def __init__(self,nome,artista,link):
+        self.nome = nome
+        self.artista = artista
+        self.link = link
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    musica = Musica('SET DJ PEDRO 6.0',
+        'MC Don Juan, MC Davi, MC Pedrinho, MC Hariel e MC Ryan SP',
+        '')
+    return render_template('index.html',musica=musica)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
